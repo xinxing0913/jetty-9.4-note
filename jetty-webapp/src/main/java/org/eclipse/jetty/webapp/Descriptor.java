a86d06395197bd5496dea1ac98eedeb5b830993e
xinguimeng <xinguimeng@163.com>
2018-02-11 14:05:44 +0800

    抽象的描述文件

--- jetty-webapp/src/main/java/org/eclipse/jetty/webapp/Descriptor.java
+++ jetty-webapp/src/main/java/org/eclipse/jetty/webapp/Descriptor.java
@@ -21,59 +21,96 @@ package org.eclipse.jetty.webapp;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.xml.XmlParser;
 
-public abstract class Descriptor
-{
+/**
+ * 描述文件
+ */
+public abstract class Descriptor {
+
+    /**
+     * 资源文件
+     */
     protected Resource _xml;
+
+    /**
+     * 根节点
+     */
     protected XmlParser.Node _root;
+
+    /**
+     * dtd文件
+     */
     protected String _dtd;
+
+    /**
+     * 校验
+     */
     protected boolean _validating;
-    
-    public Descriptor (Resource xml)
-    {
+
+    /**
+     * 构造方法
+     *
+     * @param xml
+     */
+    public Descriptor (Resource xml) {
         _xml = xml;
     }
-    
-    public abstract XmlParser ensureParser()
-    throws ClassNotFoundException;
-    
-    
-    public void setValidating (boolean validating)
-    {
+
+    /**
+     * 确保解析通过
+     *
+     * @return
+     * @throws ClassNotFoundException
+     */
+    public abstract XmlParser ensureParser() throws ClassNotFoundException;
+
+    /**
+     * 设置是否校验
+     *
+     * @param validating
+     */
+    public void setValidating (boolean validating) {
        _validating = validating;
     }
-    
-    public void parse ()
-    throws Exception
-    {
 
-        
-        if (_root == null)
-        {
-            try
-            {
+    /**
+     * 解析
+     *
+     * @throws Exception
+     */
+    public void parse () throws Exception {
+        if (_root == null) {
+            try {
                 XmlParser parser = ensureParser();
                 _root = parser.parse(_xml.getInputStream());
                 _dtd = parser.getDTD();
-            }
-            finally
-            {
+            } finally {
                 _xml.close();
             }
         }
     }
-    
-    public Resource getResource ()
-    {
+
+    /**
+     * 获取资源
+     */
+    public Resource getResource () {
         return _xml;
     }
-    
-    public XmlParser.Node getRoot ()
-    {
+
+    /**
+     * 获取根节点
+     *
+     * @return
+     */
+    public XmlParser.Node getRoot () {
         return _root;
     }
-    
-    public String toString()
-    {
+
+    /**
+     * 转换为字符串
+     *
+     * @return
+     */
+    public String toString() {
         return this.getClass().getSimpleName()+"("+_xml+")";
     }
 }
