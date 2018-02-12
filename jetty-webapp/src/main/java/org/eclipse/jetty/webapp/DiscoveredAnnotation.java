f4fa44f27e09d2d6deb39d9ee1ce141f9486c305
xinguimeng <xinguimeng@163.com>
2018-02-12 12:25:03 +0800

    基于注解的发现方式

--- jetty-webapp/src/main/java/org/eclipse/jetty/webapp/DiscoveredAnnotation.java
+++ jetty-webapp/src/main/java/org/eclipse/jetty/webapp/DiscoveredAnnotation.java
@@ -28,61 +28,102 @@ import org.eclipse.jetty.util.resource.Resource;
  *
  * Represents an annotation that has been discovered
  * by scanning source code of WEB-INF/classes and WEB-INF/lib jars.
- * 
+ *
+ * 通过注解发现
+ * 通过扫描WEB-INF/classes或者WEB-INF/lib的jar包的代码获取
  */
-public abstract class DiscoveredAnnotation
-{
+public abstract class DiscoveredAnnotation {
+    /**
+     * 日志类
+     */
     private static final Logger LOG = Log.getLogger(DiscoveredAnnotation.class);
 
+    /**
+     * 上下文
+     */
     protected WebAppContext _context;
+
+    /**
+     * 类名
+     */
     protected String _className;
+
+    /**
+     * 类
+     */
     protected Class<?> _clazz;
-    protected Resource _resource; //resource it was discovered on, can be null (eg from WEB-INF/classes)
-    
+
+    /**
+     * resource it was discovered on, can be null (eg from WEB-INF/classes)
+     *
+     * 资源
+     */
+    protected Resource _resource;
+
+    /**
+     * 提供
+     */
     public abstract void apply();
-    
-    public DiscoveredAnnotation (WebAppContext context, String className)
-    {
+
+    /**
+     * 构造方法
+     *
+     * @param context
+     * @param className
+     */
+    public DiscoveredAnnotation (WebAppContext context, String className) {
         this(context,className, null);
-    } 
-    
-    
-    public DiscoveredAnnotation(WebAppContext context, String className, Resource resource)
-    {
+    }
+
+    /**
+     * 构造方法
+     *
+     * @param context
+     * @param className
+     * @param resource
+     */
+    public DiscoveredAnnotation(WebAppContext context, String className, Resource resource) {
         _context = context;
         _className = className;
         _resource = resource;
     }
-    
-    public Resource getResource ()
-    {
+
+    /**
+     * 获取资源
+     *
+     * @return
+     */
+    public Resource getResource () {
         return _resource;
     }
-    
-    public Class<?> getTargetClass()
-    {
-        if (_clazz != null)
+
+    /**
+     * 获取目标类
+     */
+    public Class<?> getTargetClass() {
+        if (_clazz != null) {
             return _clazz;
+        }
         
         loadClass();
         
         return _clazz;
     }
-    
-    private void loadClass ()
-    {
-        if (_clazz != null)
+
+    /**
+     * 加载类
+     */
+    private void loadClass () {
+        if (_clazz != null) {
             return;
+        }
         
         if (_className == null)
             return;
         
-        try
-        {
+        try {
             _clazz = Loader.loadClass(_className);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             LOG.warn(e);
         }
     }  
