6e874f93fd1c9d99de5bfed87ac0acd240ee48a4
xinguimeng <xinguimeng@163.com>
2018-02-12 17:21:50 +0800

    描述文件解析器

--- jetty-webapp/src/main/java/org/eclipse/jetty/webapp/IterativeDescriptorProcessor.java
+++ jetty-webapp/src/main/java/org/eclipse/jetty/webapp/IterativeDescriptorProcessor.java
@@ -27,45 +27,77 @@ import org.eclipse.jetty.xml.XmlParser;
 
 /**
  * IterativeDescriptorProcessor
+ *
+ * 可迭代的
  */
-public abstract class IterativeDescriptorProcessor implements DescriptorProcessor
-{
-    public static final Class<?>[] __signature = new Class[]{WebAppContext.class, Descriptor.class, XmlParser.Node.class};
+public abstract class IterativeDescriptorProcessor implements DescriptorProcessor {
+
+    /**
+     * 签名
+     */
+    public static final Class<?>[] __signature = new Class[]{
+            WebAppContext.class,
+            Descriptor.class,
+            XmlParser.Node.class
+    };
+
+
+    /**
+     * 访问器
+     */
     protected Map<String, Method> _visitors = new HashMap<String, Method>();
+
+    /**
+     * 开始
+     *
+     * @param context
+     * @param descriptor
+     */
     public abstract void start(WebAppContext context, Descriptor descriptor);
+
+    /**
+     * 结束
+     *
+     * @param context
+     * @param descriptor
+     */
     public abstract void end(WebAppContext context, Descriptor descriptor);
 
     /**
      * Register a method to be called back when visiting the node with the given name.
      * The method must exist on a subclass of this class, and must have the signature:
      * public void method (Descriptor descriptor, XmlParser.Node node)
+     *
+     * 注册访问者
+     *
      * @param nodeName the node name
      * @param m the method name
      */
-    public void registerVisitor(String nodeName, Method m)
-    {
+    public void registerVisitor(String nodeName, Method m) {
         _visitors.put(nodeName, m);
     }
 
     
-    /** 
+    /**
+     * 执行
+     *
      * {@inheritDoc}
      */
-    public void process(WebAppContext context, Descriptor descriptor)
-    throws Exception
-    {
-        if (descriptor == null)
+    public void process(WebAppContext context, Descriptor descriptor) throws Exception {
+        if (descriptor == null) {
             return;
+        }
 
         start(context,descriptor);
 
         XmlParser.Node root = descriptor.getRoot();
         Iterator<?> iter = root.iterator();
         XmlParser.Node node = null;
-        while (iter.hasNext())
-        {
+        while (iter.hasNext()) {
             Object o = iter.next();
-            if (!(o instanceof XmlParser.Node)) continue;
+            if (!(o instanceof XmlParser.Node)) {
+                continue;
+            }
             node = (XmlParser.Node) o;
             visit(context, descriptor, node);
         }
@@ -73,13 +105,19 @@ public abstract class IterativeDescriptorProcessor implements DescriptorProcesso
         end(context,descriptor);
     }
 
-
-    protected void visit (WebAppContext context, Descriptor descriptor, XmlParser.Node node)
-    throws Exception
-    {
+    /**
+     * 访问
+     *
+     * @param context
+     * @param descriptor
+     * @param node
+     * @throws Exception
+     */
+    protected void visit (WebAppContext context, Descriptor descriptor, XmlParser.Node node) throws Exception {
         String name = node.getTag();
         Method m =  _visitors.get(name);
-        if (m != null)
+        if (m != null) {
             m.invoke(this, new Object[]{context, descriptor, node});
+        }
     }
 }
