d5a338b704255861f1d6d61f383d198ed3b574f5
xinguimeng <xinguimeng@163.com>
2018-02-13 15:35:53 +0800

    基于nosql的session存储

--- jetty-nosql/src/main/java/org/eclipse/jetty/nosql/NoSqlSessionDataStore.java
+++ jetty-nosql/src/main/java/org/eclipse/jetty/nosql/NoSqlSessionDataStore.java
@@ -29,61 +29,102 @@ import org.eclipse.jetty.server.session.SessionData;
 /**
  * NoSqlSessionDataStore
  *
- *
+ * 使用nosql来存储session的基类
  */
-public abstract class NoSqlSessionDataStore extends AbstractSessionDataStore
-{
-    
-    public class NoSqlSessionData extends SessionData
-    {
-        private Object _version;
-        private Set<String> _dirtyAttributes = new HashSet<String>();
-        
+public abstract class NoSqlSessionDataStore extends AbstractSessionDataStore {
 
-        public NoSqlSessionData(String id, String cpath, String vhost, long created, long accessed, long lastAccessed, long maxInactiveMs)
-        {
+    /**
+     * 基于nosql存储的session数据
+     */
+    public class NoSqlSessionData extends SessionData {
+        /**
+         * 版本
+         */
+        private Object _version;
+
+        /**
+         * 脏属性
+         */
+        private Set<String> _dirtyAttributes = new HashSet<String>();
+
+        /**
+         * 构造方法
+         *
+         * @param id
+         * @param cpath
+         * @param vhost
+         * @param created
+         * @param accessed
+         * @param lastAccessed
+         * @param maxInactiveMs
+         */
+        public NoSqlSessionData(String id, String cpath, String vhost, long created, long accessed, long lastAccessed, long maxInactiveMs) {
             super(id, cpath, vhost, created, accessed, lastAccessed, maxInactiveMs);
         }
-        
-        public void setVersion (Object v)
-        {
+
+        /**
+         * 设置版本
+         *
+         * @param v
+         */
+        public void setVersion (Object v) {
             _version = v;
         }
-        
-        public Object getVersion ()
-        {
+
+        /**
+         * 获取版本
+         *
+         * @return
+         */
+        public Object getVersion () {
             return _version;
         }
 
+        /**
+         * 标记为脏
+         *
+         * @param name
+         */
         @Override
-        public void setDirty(String name)
-        {
+        public void setDirty(String name) {
             super.setDirty(name);
             _dirtyAttributes.add(name);
         }
-        
-        
-        public Set<String> takeDirtyAttributes()
-        {
+
+        /**
+         * 获取所有的脏属性
+         *
+         * @return
+         */
+        public Set<String> takeDirtyAttributes() {
             Set<String> copy = new HashSet<>(_dirtyAttributes);
             _dirtyAttributes.clear();
             return copy;
-            
         }
-        
-        public Set<String> getAllAttributeNames ()
-        {
+
+        /**
+         * 获取所有的属性名
+         *
+         * @return
+         */
+        public Set<String> getAllAttributeNames () {
             return new HashSet<String>(_attributes.keySet());
         }
     }
 
-
+    /**
+     * 获取一个新的SessionData
+     *
+     * @param id
+     * @param created
+     * @param accessed
+     * @param lastAccessed
+     * @param maxInactiveMs
+     * @return
+     */
     @Override
-    public SessionData newSessionData(String id, long created, long accessed, long lastAccessed, long maxInactiveMs)
-    {
+    public SessionData newSessionData(String id, long created, long accessed, long lastAccessed, long maxInactiveMs) {
         return new NoSqlSessionData(id, _context.getCanonicalContextPath(), _context.getVhost(), created, accessed, lastAccessed, maxInactiveMs);
     }
-    
-    
 
 }
